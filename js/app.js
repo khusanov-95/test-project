9f6c8c8a66f835ff4d33b712f0468533653c7a3f
khusanov-95 <67969838+khusanov-95@users.noreply.github.com>
2020-11-04 21:51:18 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -65,11 +65,35 @@ $(document).ready(function(){
    $( ".fixed-header" ).css( "display", "none" ); 
     } 
   }); 
-  //m menu btn 
-  $('.small-nav-btn').on('click', function(){
-    $('.nav-small').toggleClass('.nav-small--active')
-  });
 
 });
+//vanilla js
 
+//mobile menu
+const btn = document.querySelector('.small-nav-btn');
+const container = document.querySelector('.nav-small');
+const icon = document.querySelector('.small-nav-btn')
 
+btn.addEventListener('click', () => {
+  container.classList.toggle('d-none');
+  if(container.classList.contains('d-none')){
+    icon.innerHTML = `<i class="fas fa-bars"></i>`;
+  } else {
+    icon.innerHTML = `<i class="fas fa-times"></i>`;
+    btn.style.top = '2%';
+  }
+})
+
+//modal
+const calls = document.querySelectorAll('.callme');
+const closeCall = document.querySelector('.close-modal');
+const modal = document.querySelector('.callme-container')
+
+calls.forEach((call) => {
+  call.addEventListener('click', () => {
+    modal.style.display="flex";
+  });
+});
+closeCall.addEventListener('click', () => {
+  modal.style.display="none";
+})
